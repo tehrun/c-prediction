b0e9ce59eae9089d9ca4ead480e42a3080854cc6
Amir <80174515+tehrun@users.noreply.github.com>
2026-07-10 10:23:00 +0200

    Fix Alembic async database migrations

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -1,6 +1,7 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config, pool
+from sqlalchemy import pool
+from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 from app import models  # noqa: F401
@@ -8,7 +9,7 @@ from app.core.config import get_settings
 from app.db.base import Base
 
 config = context.config
-config.set_main_option("sqlalchemy.url", get_settings().database_url.replace("+asyncpg", ""))
+config.set_main_option("sqlalchemy.url", get_settings().database_url)
 if config.config_file_name:
     fileConfig(config.config_file_name)
 target_metadata = Base.metadata
@@ -25,13 +26,24 @@ def run_migrations_offline():
 
 
 def run_migrations_online():
-    connectable = engine_from_config(
+    connectable = async_engine_from_config(
         config.get_section(config.config_ini_section), prefix="sqlalchemy.", poolclass=pool.NullPool
     )
-    with connectable.connect() as connection:
-        context.configure(connection=connection, target_metadata=target_metadata)
-        with context.begin_transaction():
-            context.run_migrations()
+
+    async def do_run_migrations() -> None:
+        async with connectable.connect() as connection:
+            await connection.run_sync(run_sync_migrations)
+        await connectable.dispose()
+
+    import asyncio
+
+    asyncio.run(do_run_migrations())
+
+
+def run_sync_migrations(connection):
+    context.configure(connection=connection, target_metadata=target_metadata)
+    with context.begin_transaction():
+        context.run_migrations()
 
 
 if context.is_offline_mode():
